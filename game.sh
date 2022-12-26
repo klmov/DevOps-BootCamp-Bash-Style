cac273a5fa9a34ca3bb4fe916d91427977dc8d03
Ranjit Kumar <95127402+ravikpt@users.noreply.github.com>
2022-12-26 18:23:54 +0400

    Update game.sh

--- game.sh
+++ game.sh
@@ -2,37 +2,41 @@
 
 echo -e "\n NumberJack \n"
 ch=0
-while [ $ch -ne 3 ]; do
+while [ "$ch" -ne 3 ]; do
     echo  "  
          PLAY : Hit 1 and enter.
          HELP : Hit 2 and enter.
          EXIT : Hit 3 and enter.
          "
-    read -r "Enter your choice : " ch
-    if [ "$ch" -eq 1 ];then
-    x=0 ;c=0 ;p=0
-    read -r "Enter any number between 0 and 9 : " n
-    while [ "$c" -eq 0 ];
-    do
-        x=11; r=("$(shuf -i 0-9 -n 10)")
-        echo "${r[*]} "; for i in {1..10}; do
+    read -r -p "Enter your choice : " ch
+    if [ "$ch" -eq 1 ]; then
+    x=0
+    c=0
+    p=0
+    read -r -p "Enter any number between 0 and 9 : " n
+    while [ "$c" -eq 0 ]; do
+        x=11
+	r=()
+	mapfile -t r < <(shuf -i 0-9 -n 10)
+        echo "${r[@]}"
+	for i in {1..10}; do
             a[$i]=$i
         done
-        echo "${a[*]} "
-        read -t 5 -r "Enter the index of your number : " x
+        echo "${a[*]}"
+        read -r -t 5 -p "Enter the index of your number : " x
     if [[ $? -gt 128 ]]; then
-    c=1
-    break
+      c=1
+      break
     fi
-if [ "${r[$x-1]}" -eq "$n" ];then
-            echo "Great"
-    ((p=p+1))
-        else
+    if [[ "${r[(($x)) - 1}" -eq "$n" ]];then
+       echo "Great"
+       p=$( (p+1) )
+       else
             c=1
             break
         fi
     done
-    elif [ "$ch" -eq 2 ];then
+    elif [ "$ch" -eq 2 ]; then
         echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
 else
         break
@@ -41,5 +45,5 @@ if [ "$c" -eq 1 ];then
             echo -e "\nGAME OVER\n"
             echo "You scored $p points"
 fi
-        done
+done
 
